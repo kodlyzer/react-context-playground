5c42130e77297222febd0d8b0b0e2146246ee09e
Karthik Rana <kodlyzer@gmail.com>
2022-10-18 12:21:13 +0530

    Cleanup

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,9 +2,7 @@ import DashboardProvider from '../utils/dashboard.context'
 import DCard from './DCard'
 
 const Dashboard = ({ accountDetails }) => {
-    debugger;
     return (
-
         <div className='grid grid-cols-3 auto-rows-auto gap-8 px-4'>
             {accountDetails?.map((item, index) => <DCard title={item.title} value={item.value} key={index} />)}
         </div>
